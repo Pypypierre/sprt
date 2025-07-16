d3739629d48e095564bda4d6d1744a82094f4d9c
Pypypierre <easychinese76@gmail.com>
2025-07-16 17:36:02 +0200

    feat: enhacement paraph

--- src/lib/components/Values.svelte
+++ src/lib/components/Values.svelte
@@ -3,7 +3,7 @@
 	<h2 class="text-4xl text-[#F9B333] font-bold mb-6">
 	  Une vision, des valeurs, votre confiance
 	</h2>
-	<div class="text-gray-300 text-lg md:text-xl leading-relaxed space-y-4">
+	<div id='paraph' class="space-y-4">
 		<p class="text-gray-300 text-xl md:text-xl leading-relaxed space-y-4">L’approche de SPRT Society repose sur trois piliers : la transparence, un cadre juridique clair et conforme au droit français, et une équipe accessible en direct.</p>
 		<p class="text-gray-300 text-xl md:text-xl leading-relaxed space-y-4">Vous restez à tout moment maître de votre démarche. Nous ne requérons qu’un seuil initial de 200 € sur votre compte de trading, sans abonnement ni frais.</p>
 		<p class="text-gray-300 text-xl md:text-xl leading-relaxed space-y-4">Notre rémunération est exclusivement basée sur une commission appliquée qu'aux bénéfices.</p>
