92cde72cdffc4d186637218326057190a4b3b939
Pypypierre <easychinese76@gmail.com>
2025-09-22 19:00:31 +0200

    fix: correct the size of the logo

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -4,6 +4,6 @@
 
 {#if show}
   <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50 w-full h-full transition-opacity duration-1000">
-    <img src="/sprt-logo.png" alt="Loading..." class="w-[20%] laptop:w-[10%] mx-auto animate-fade-in-out" />
+    <img src="/sprt-logo.png" alt="Loading..." class="w-[30%] tablet:w-[20%] mx-auto animate-fade-in-out" />
   </div>
 {/if}
