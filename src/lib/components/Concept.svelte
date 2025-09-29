e37b637834ed87b05a97b20853d8fe0ba4838ca1
Pypypierre <easychinese76@gmail.com>
2025-09-29 16:50:35 +0200

    fix: correct orthograph error

--- src/lib/components/Concept.svelte
+++ src/lib/components/Concept.svelte
@@ -5,7 +5,7 @@
     </h2>
     <div id='paraph' class="text-gray-300 text-lg space-y-4">
     	<p class="text-gray-300 text-xl md:text-xl leading-relaxed space-y-4">Le monde des cryptos est pour beaucoup synonyme de manque de transparence et de peu de lisibilité sur les statuts ou le mode de fonctionnement.</p>
-		<p class="text-gray-300 text-xl md:text-xl leading-relaxed space-y-4">SPRT Society analyse le cours du Bitcoin et prend des trades pour son compte suivant une stratégie spécifique/ élabirées par l’équipe.</p>
+		<p class="text-gray-300 text-xl md:text-xl leading-relaxed space-y-4">SPRT Society analyse le cours du Bitcoin et prend des trades pour son compte suivant une stratégie spécifique, élaborée par l’équipe financière.</p>
 		<p class="text-gray-300 text-xl md:text-xl leading-relaxed space-y-4">Cette stratégie vous est communiquée automatiquement par une clé API.</p>
   	</div>
 	<a href="/concept" class="inline-block  mt-6 px-6 py-3 border border-[#F9B333] bg-[#F9B333] text-black rounded-full hover:bg-black hover:text-white transition">
