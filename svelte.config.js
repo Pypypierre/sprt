eb4af43cf7e2263346e2db7cc22fa0473e724e75
Pypypierre <easychinese76@gmail.com>
2025-10-04 17:07:59 +0200

    feat: prepare to post phase of website

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			strict: false
+		}),
+		prerender: { entries: ['*'] }
+	 },
 };
 
 export default config;
